934b7d4e12bb7f2fe303eba866763d74d403025c
Sulfur <rc7604@GDD-18.main.ad.rit.edu>
2015-04-15 18:30:00 -0400

    Memories fixed
    The memory class was missing the things we worked on so I re-added the
    things to the memory class.

--- Memories.cs
+++ Memories.cs
@@ -11,7 +11,52 @@ using Microsoft.Xna.Framework.GamerServices;
 
 namespace UGWProjCode
 {
-    class Memories
+    class Memories:GamePiece
     {
+
+        //attributes
+        private int totMem; //the total memories in that level
+        private bool advanceLevel;//if all the memories have been collected, this will be true and advance on
+
+        //properties
+        public int TotMem
+        {
+            get { return totMem; }
+            set { totMem = value; }
+        }
+
+        public bool AdvanceLevel
+        {
+            get { return advanceLevel; }
+        }
+
+        //constructor
+        public Memories(int numofmemories, Rectangle memorect, Texture2D memotexture)
+            : base(memorect, memotexture)
+        {
+            totMem = numofmemories;
+        }
+
+        /// <summary>
+        /// When a player collides with a memory, it will add it to the memory counter
+        /// </summary>
+        /// <param name="playr"></param>
+        public void AddMemory(Player playr)
+        {
+
+            playr.MemsColl++;
+        }
+
+        public void memsAllCollected(Player plr)
+        {
+            if (plr.MemsColl == totMem)
+            {
+                advanceLevel = true;
+            }
+            else
+            {
+                advanceLevel = false;
+            }
+        }
     }
 }
