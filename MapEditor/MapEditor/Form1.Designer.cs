c8551b8bcacc967bdab905447f789feccff67f08
nsun6145 <njs6145@g.rit.edu>
2015-04-21 23:36:25 -0400

    Map editor has been placed inside project folder
    If it doesn't work properly, I promise to get it fixed asap

---
+++ MapEditor/MapEditor/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace MapEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapBox = new System.Windows.Forms.TextBox();
+            this.numBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mapBox
+            // 
+            this.mapBox.Location = new System.Drawing.Point(35, 89);
+            this.mapBox.Multiline = true;
+            this.mapBox.Name = "mapBox";
+            this.mapBox.Size = new System.Drawing.Size(161, 195);
+            this.mapBox.TabIndex = 0;
+            // 
+            // numBox
+            // 
+            this.numBox.Location = new System.Drawing.Point(35, 30);
+            this.numBox.Name = "numBox";
+            this.numBox.Size = new System.Drawing.Size(50, 20);
+            this.numBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Level No.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Level Map";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(35, 302);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exportButton
+            // 
+            this.exportButton.Location = new System.Drawing.Point(116, 302);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(80, 23);
+            this.exportButton.TabIndex = 5;
+            this.exportButton.Text = "Export";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 334);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numBox);
+            this.Controls.Add(this.mapBox);
+            this.Name = "Form1";
+            this.Text = "Map Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mapBox;
+        private System.Windows.Forms.TextBox numBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exportButton;
+    }
+}
+
---
+++ MapEditor/MapEditor/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MapEditor
+{
+    public partial class Form1 : Form
+    {
+        public int lvlNum;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            numBox.Text = null;
+            mapBox.Text = null;
+        }
+
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+           
+
+
+           if (numBox.Text != " " || mapBox.Text != " ")
+            {
+                string filename = "level" + numBox.Text + ".txt";
+                StreamWriter writer = new StreamWriter(filename, false);
+                Directory.Move(@"F:\Debug", @"F:\UGWCodeProj - Copy\bin\WindowsGL\Debug");
+                writer.WriteLine(mapBox.Text);
+                
+                writer.Close();
+
+                MessageBox.Show("Level has been created.");
+            }
+
+           else if (numBox.Text == " " || mapBox.Text == " ")
+            {
+                MessageBox.Show("There are empty boxes. Please fill them out.");
+                return;
+            }
+        }
+    }
+}
